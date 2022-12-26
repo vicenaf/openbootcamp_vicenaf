3059b2b2bd1e2886feefa3071a3e61344f029654
vicenaf <42248254+vicenaf@users.noreply.github.com>
2022-12-26 10:25:54 +0100

    Add files via upload

---
+++ 001suma/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main (String[] args) {
+        System.out.println( suma (1, 2 , 3));
+    }
+
+    public static int suma (int a, int b, int c) {
+
+        return a + b + c;
+    }
+}
+
+
+
